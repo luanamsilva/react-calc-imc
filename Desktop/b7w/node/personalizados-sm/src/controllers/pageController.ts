8e2ac6d1f10c55e07f6f1d5ed1863f00bae81152
luanamsilva <luanamoura2002@yahoo.com.br>
2022-12-01 09:51:53 -0300

    base created

---
+++ Desktop/b7w/node/personalizados-sm/src/controllers/pageController.ts
@@ -0,0 +1,24 @@
+import { Request, Response } from "express";
+
+export const home = (req:Request, res:Response) =>{
+res.send('teste')
+}
+
+export const fullParty = (req:Request, res:Response) =>{
+
+}
+
+export const stationary = (req:Request, res:Response) =>{
+
+}
+
+export const lux = (req:Request, res:Response) =>{
+
+}
+export const kitkat = (req:Request, res:Response) =>{
+
+}
+
+export const special = (req:Request, res:Response) =>{
+
+}
